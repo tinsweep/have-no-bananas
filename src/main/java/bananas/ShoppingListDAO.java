ad45f5b4c5a8e61da81f3cb71eba093ea916f997
Bryan Thetford <bryan.thetford@gmail.com>
2014-06-08 18:42:31 -0500

    updated package names

---
+++ src/main/java/bananas/ShoppingListDAO.java
@@ -0,0 +1,30 @@
+package bananas;
+
+import java.util.List;
+
+public class ShoppingListDAO implements ListOfItemsDAO{
+	
+	@Override
+	public List<ListOfItems>getAllLists(){
+		//search through database for any shopping lists and
+		//return them as an Arraylist
+		
+	}
+	public void saveListOfItems(ListOfItems shpList){
+		//create connection and add object to the database
+	}
+	
+	@Override
+	public List<ListItem> getListOfItems(String name){
+		//return shopping list with name s
+	}
+	
+	@Override
+	public List<ListItem> addItemToList(FoodItem foodItem){
+		
+		
+	}
+
+	
+
+}

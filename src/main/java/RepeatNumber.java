1fd27ca9e42a0250522aab7cae3f0632f9f4b1c6
niffer01@users.noreply.github.com <niffer01@gmail.com>
2014-06-08 12:09:39 -0400

    Added trivial Java classes for check ant build.

---
+++ src/main/java/RepeatNumber.java
@@ -0,0 +1,17 @@
+package bananas;
+
+import java.lang.String;
+
+public class RepeatNumber {
+    private String firstWord;
+    private String secondWord;
+
+    public RepeatNumber(String one, String two){
+        firstWord = one.toString();
+        secondWord = two.toString();
+    }
+
+    public String hello(){
+        return firstWord().toString();
+        }
+}
---
+++ src/main/java/bananas/RepeatNumber.java
@@ -0,0 +1,17 @@
+package bananas;
+
+import java.lang.String;
+
+public class RepeatNumber {
+    private int firstInt;
+    private int secondInt;
+
+    public RepeatNumber(int first, int second){
+        firstInt = first;
+        secondInt = second;
+    }
+
+    public int giveFirst(){
+        return firstInt;
+        }
+}

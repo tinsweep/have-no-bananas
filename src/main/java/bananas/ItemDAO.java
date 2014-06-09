e28a0c217dcfd3819054a1d81fd8e925daa3229b
Bryan Thetford <bryan.thetford@gmail.com>
2014-06-08 19:04:39 -0500

    Updated package names

--- src/main/java/bananas/ItemDAO.java
+++ src/main/java/bananas/ItemDAO.java
@@ -1,9 +1,9 @@
-package main;
+package bananas;
 
 public interface ItemDAO {
 	public List<FoodItem> getAllItems();
 	public FoodItem getFoodItem(String s);
 	public void updateItem(FoodItem i);
-	public void deleteItem();
+	public void deleteItem(FoodItem i);
 
 }

3bdd23ceea77f87f86558ac8282db391a5969e72
niffer01@users.noreply.github.com <niffer01@gmail.com>
2014-06-15 21:08:22 -0400

    Created abstract class Item.  Changed FoodItem to extend Item.  Added test cases to FoodItemTest.  Removed RepeatNumber and RepeatNumberTest.

--- src/main/java/bananas/FoodItem.java
+++ src/main/java/bananas/FoodItem.java
@@ -3,32 +3,35 @@ package bananas;
 /**
  * Created by Jen on 6/9/2014.
  */
-public class FoodItem implements FoodInfo {
+public class FoodItem extends Item {
 
     private String name;
+    private String category;
 
+    //@param String FoodItem name
     public FoodItem(String s){
         name = s;
     }
-/*
-    public void FoodItem(String s){
 
-        name = s;
+    //@param String FoodItem name, String FoodItem category
+    public FoodItem(String n, String c){
+        name = n;
+        category = c;
     }
-  */
-    public void setName(String name) {
 
+    public void setName(String n) {
+        name = n;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setCategory(String category) {
-
+    public void setCategory(String c) {
+        category = c;
     }
 
     public String getCategory() {
-        return null;
+        return category;
     }
 }
---
+++ src/main/java/bananas/Item.java
@@ -0,0 +1,12 @@
+package bananas;
+
+/**
+ * Created by Jen on 6/15/2014.
+ */
+abstract class Item {
+
+    abstract void setName(String n);
+    abstract String getName();
+    abstract void setCategory(String c);
+    abstract String getCategory();
+}

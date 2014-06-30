71b902768b6e99e6d54be563c884a770c7f1889f
Bryan Thetford <bryan.thetford@gmail.com>
2014-06-29 23:08:48 -0500

    Equals and hashCode in ListItem and FoodItem overridden.

--- src/main/java/bananas/FoodItem.java
+++ src/main/java/bananas/FoodItem.java
@@ -1,34 +1,72 @@
-package bananas;
+package main.java.bananas;
 
 /**
  * Created by Jen on 6/9/2014.
  */
-public class FoodItem implements FoodInfo {
+public class FoodItem extends Item {
 
     private String name;
+    private String category;
 
+    //@param String FoodItem name
     public FoodItem(String s){
         name = s;
     }
-/*
-    public void FoodItem(String s){
 
-        name = s;
+    //@param String FoodItem name, String FoodItem category
+    public FoodItem(String n, String c){
+        name = n;
+        category = c;
     }
-  */
-    public void setName(String name) {
 
+    public void setName(String n) {
+        name = n;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setCategory(String category) {
-
+    public void setCategory(String c) {
+        category = c;
     }
 
     public String getCategory() {
-        return null;
+        return category;
     }
+    /* Author: Bryan Thetford
+     * Date: 6/23/14
+  */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((category == null) ? 0 : category.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+    /* Author: Bryan Thetford
+     * Date: 6/23/14
+  */
+	   @Override
+	    public boolean equals(Object item){
+		   if(this == item)
+			   return true;
+		   if(item == null)
+			   return false;
+		   if(!(item instanceof FoodItem))
+	    		return false;
+		   FoodItem itemToCompare = (FoodItem) item;
+		   if(name.equals(itemToCompare.name) && category.equals(itemToCompare.category))
+			   return true;
+		   else
+			   return false;
+	    }
+    
 }
+
+
+
+
--- src/main/java/bananas/ListItem.java
+++ src/main/java/bananas/ListItem.java
@@ -1,42 +1,163 @@
-package bananas;
+package main.java.bananas;
 
 /**
  * Created by Jen on 6/9/2014.
  */
-public class ListItem implements ListItemInfo{
+public class ListItem extends Item {
+
+    private String name;
+    private Double quantity;
+    private String category;
+    private String unit;
+    private Double price;
+    private FoodItem foodItem;
+
+    public static class CreateListItem {
+
+        //Required parameter
+        private final FoodItem foodItem;
+
+        //Optional parameters
+        private String name;
+        private Double quantity;
+        private String category;
+        private String unit;
+        private Double price;
+
+        public CreateListItem(FoodItem val){
+            foodItem = val;
+        }
+
+        public CreateListItem name(String val){
+            name = val;
+            return this;
+        }
+
+        public CreateListItem quantity(Double val){
+            quantity = val;
+            return this;
+        }
+
+        public CreateListItem category(String val){
+            category = val;
+            return this;
+        }
+
+        public CreateListItem unit(String val){
+            unit = val;
+            return this;
+        }
+
+        public CreateListItem price(Double val){
+            price = val;
+            return this;
+        }
+
+        public ListItem create(){
+            return new ListItem(this);
+        }
+    }
+
+    private ListItem(CreateListItem listItemData){
+        foodItem = listItemData.foodItem;
+        name = listItemData.name;
+        quantity = listItemData.quantity;
+        category = listItemData.category;
+        unit = listItemData.unit;
+        price = listItemData.price;
+    }
+
     public String getName() {
-        return null;
+        return foodItem.getName();
+    }
+    
+    public void setName(String name){
+    	this.name = name;
     }
 
     public void setQuantity(Double q) {
-
+        quantity = q;
     }
 
     public Double getQuantity() {
-        return null;
+        return quantity;
     }
 
-    public void setUnit(String unit) {
-
+    public void setUnit(String u) {
+        unit = u;
     }
 
     public String getUnit() {
-        return null;
+        return unit;
     }
 
-    public void setPrice(Double price) {
-
+    public void setPrice(Double p) {
+        price = p;
     }
 
     public Double getPrice() {
-        return null;
+        return price;
     }
 
-    public void setCategory(String category) {
-
+    public void setCategory(String c) {
+        category = c;
     }
 
     public String getCategory() {
-        return null;
+        return category;
     }
+
+    
+
+    public FoodItem getFoodItem(){
+    	return foodItem;
+    }
+    /* Author: Bryan Thetford
+     * Date: 6/23/14
+  */
+    @Override
+    public boolean equals(Object obj){
+    	if(this == obj)
+    		return true;
+    	//if arg obj is null return false
+    	if(obj == null)
+    		return false;
+    	if(!(obj instanceof ListItem))
+    		return false;
+    	//cast obj to ListItem
+    	ListItem li = (ListItem)obj;
+    	//if any fields are not equal return true
+    	if(!(foodItem.equals(li.foodItem)))
+    		return false;
+    	if(!(name.equals(li.name)))
+    		return false;
+    	if(!(price.equals(li.price)))
+    		return false;
+    	if(!(quantity.equals(li.quantity)))
+    		return false;
+    	if(!(unit.equals(li.unit)))
+    		return false;
+    	if(!(category.equals(li.category)))
+    		return false;
+    	//else
+    	return true;
+    }   
+    /* Author: Bryan Thetford
+     * Date: 6/23/14
+  */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((category == null) ? 0 : category.hashCode());
+		result = prime * result
+				+ ((foodItem == null) ? 0 : foodItem.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((price == null) ? 0 : price.hashCode());
+		result = prime * result
+				+ ((quantity == null) ? 0 : quantity.hashCode());
+		result = prime * result + ((unit == null) ? 0 : unit.hashCode());
+		return result;
+	}
 }

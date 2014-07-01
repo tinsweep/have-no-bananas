4bde7ee4d3d31d90813a3923a8d39ad46990be88
spencermarks <spencer.marks@claritasgenomics.com>
2014-07-01 10:16:36 -0400

    add some comments

--- src/main/java/bananas/FoodItem.java
+++ src/main/java/bananas/FoodItem.java
@@ -1,25 +1,30 @@
 package bananas;
 
 /**
+ *
+ * todo be sure to javadoc all classes
+ *
  * Created by Jen on 6/9/2014.
  */
 public class FoodItem implements FoodInfo {
 
     private String name;
 
-    public FoodItem(String s){
+    public FoodItem(String s) {
         name = s;
     }
-/*
-    public void FoodItem(String s){
 
-        name = s;
-    }
-  */
+    /*
+        public void FoodItem(String s){
+
+            name = s;
+        }
+      */
     public void setName(String name) {
 
     }
 
+    // todo be sure to javadoc all methods
     public String getName() {
         return name;
     }
